3f44038a1dbfd32c535d340eb82bc0f74f6743ee
maderisza <maderisza@gmail.com>
2023-01-17 14:14:25 +0700

    add another screen

---
+++ src/screen/Asesmen/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function Asesmen() {
+  return (
+    <View>
+      <Text>Asesmen Screen</Text>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({});
---
+++ src/screen/Verifikasi/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export default function Verifikisai(props) {
+  return (
+    <>
+      <Text>Verifikisai Screen</Text>
+    </>
+  );
+}
