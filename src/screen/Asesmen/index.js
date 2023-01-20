19cc5faf4c73fb18307ce28338923d2dcdbdefed
maderisza <maderisza@gmail.com>
2023-01-20 15:06:00 +0700

    add screen asesmen

--- src/screen/Asesmen/index.js
+++ src/screen/Asesmen/index.js
@@ -30,8 +30,184 @@ export default function Asesmen() {
             <Text>Lapor Bencana</Text>
             <Text>Perbaiki Isian Data Bencana</Text>
           </View>
-          <View>
+          <View style={{marginTop: 10}}>
             <Text>Jenis Bencana</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Tanggal Kejadian</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Titik Lokasi Terjadinya Bencana</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Kecamatan</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Desa</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Alamat</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Tanggal Asesmen</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Dampak</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Kerusakan Fasum</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Kerusakan Rumah</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Kerugian</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Korban Luka</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Korban Jiwa</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Cakupan Bencana</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Potensi Bencana Susulan</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Deskripsi Kronologis</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Tindakan akan dilakukan</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Peralatan yang dibutuhkan</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Kebutuhan mendesak</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Unsur terlibat</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Petugas</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Data Barang Yang Di Butuhkan</Text>
+            <TextInput
+              placeholder="Tanah Longsor"
+              editable={false}
+              style={{borderWidth: 1, borderRadius: 10}}
+            />
+          </View>
+          <View style={{marginTop: 10}}>
+            <Text>Upload File Gambar</Text>
             <TextInput
               placeholder="Tanah Longsor"
               editable={false}
@@ -57,7 +233,7 @@ const style = StyleSheet.create({
     borderRadius: 30,
     padding: 6,
     width: '100%',
-    height: 500,
+    height: 2000,
     position: 'relative',
     marginTop: -10,
   },
