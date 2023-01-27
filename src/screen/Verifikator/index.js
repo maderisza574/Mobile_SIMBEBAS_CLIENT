3a61b53d20ed0a1b21be8bd0704df096c75fef4a
maderisza <maderisza@gmail.com>
2023-01-27 15:18:14 +0700

    add screen verifikator

--- src/screen/Verifikator/index.js
+++ src/screen/Verifikator/index.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, ScrollView, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/Entypo';
 
 export default function Verifikator() {
   return (
     <View>
-      <Text>Verifikator</Text>
+      <ScrollView>
+        <View style={style.titleScreen}>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: -3,
+            }}>
+            <Icon
+              name="news"
+              size={40}
+              color={'white'}
+              style={{marginLeft: 10}}
+            />
+            <Text style={{color: 'white'}}>Verifikator</Text>
+          </View>
+        </View>
+        <Text>Verifikator</Text>
+      </ScrollView>
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  titleScreen: {
+    backgroundColor: '#FF6A16',
+    color: 'white',
+    height: 100,
+  },
+});
